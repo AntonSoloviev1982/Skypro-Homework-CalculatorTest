2400f05eaaaf888727c7866aba2601988cefdfa0
Anton <Soloviev.gvardiya@gmail.com>
2023-05-09 16:34:55 +0300

    Первый коммит

---
+++ src/main/java/pro/sky/skyprohomeworkcalculatortest/SkyproHomeworkCalculatorTestApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.skyprohomeworkcalculatortest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproHomeworkCalculatorTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkyproHomeworkCalculatorTestApplication.class, args);
+    }
+
+}
